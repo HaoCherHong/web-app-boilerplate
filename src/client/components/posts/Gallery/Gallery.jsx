a034e37835e6ddddf1469c19a9ad5156fd19531a
Hao-Cher Hong <rax333j@gmail.com>
2018-02-03 00:40:32 +0800

    * Fixed Gallery

--- src/client/components/posts/Gallery/Gallery.jsx
+++ src/client/components/posts/Gallery/Gallery.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import styles from './Gallery.css';
 
 export default class Gallery extends React.Component {
-	state = {
+  state = {
     currentIndex: 0
   };
 
-	render() {
-		const {post} = this.props;
+  render() {
+    const {post} = this.props;
     const {currentIndex} = this.state;
 
-		return (
-			<div className={styles.gallery}>
-				<img src={'/images/pictures/' + post.photos[currentIndex]} style={{width: '100%'}}/>
-				{
-					post.photos.length >= 2 && (
-						<div className="navigate">
-							<div className="left side-area" onClick={this.handlePreviousClick}/>
-							<div className="right side-area" onClick={this.handleNextClick}/>
-						</div>
-					)
-				}
-				{
-					post.adopted && <div className="adopted" title="已被認養"/>
-				}
-			</div>
-		)
+    return (
+      <div className={styles.gallery}>
+        <img className={styles.image} src={'/images/pictures/' + post.photos[currentIndex]}/>
+        {
+          post.photos.length >= 2 && (
+            <div className={styles.navigate}>
+              <div className={classNames(styles.left, styles.sideArea)} onClick={this.handlePreviousClick}/>
+              <div className={classNames(styles.right, styles.sideArea)} onClick={this.handleNextClick}/>
+            </div>
+          )
+        }
+        {
+          post.adopted && <div className="adopted" title="已被認養"/>
+        }
+      </div>
+    );
   }
 
   handleNextClick = e => {
