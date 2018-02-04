8fc922ce98e562ccee833b93e63b5c5c93a1b5c0
Hao-Cher Hong <rax333j@gmail.com>
2018-02-04 23:09:40 +0800

    * Fixed HMR errors

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import { Provider } from 'react-redux';
 import { compose, createStore, applyMiddleware } from 'redux';
@@ -9,31 +9,48 @@ import reducers from './reducers';
 
 import routes from './routes';
 
-let store;
-if (process.env.NODE_ENV === 'development') {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  store = createStore(reducers, composeEnhancers(
-    applyMiddleware(thunk),
-  ));
-} else {
-  store = createStore(reducers, applyMiddleware(thunk));
+function configureStore(initialState) {
+  let store;
+
+  if (process.env.NODE_ENV === 'development') {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    store = createStore(reducers, initialState, composeEnhancers(
+      applyMiddleware(thunk),
+    ));
+  } else {
+    store = createStore(reducers, applyMiddleware(thunk));
+  }
+
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers').default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
 }
 
-const AppCore = React.createElement(Provider, {store},
-  React.createElement(BrowserRouter, null, renderRoutes(routes))
-);
+const store = configureStore();
 
-const render = () => {
+function render(routes) {
+  const AppCore = React.createElement(Provider, {store},
+    React.createElement(BrowserRouter, null, renderRoutes(routes))
+  );
+
   if (process.env.NODE_ENV === 'development') {
     const AppContainer = require('react-hot-loader').AppContainer;
     ReactDOM.render(React.createElement(AppContainer, null, AppCore), document.getElementById('react-root'));
   } else {
     ReactDOM.render(AppCore, document.getElementById('react-root'));
   }
-};
-
-render();
+}
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept('./routes', () => {
+    const routes = require('./routes').default;
+    render(routes);
+  });
 }
+
+render(routes);
