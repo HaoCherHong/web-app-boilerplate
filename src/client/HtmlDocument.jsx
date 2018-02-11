770c694ed613534ce2c9105b7c9b6885399f911e
Hao-Cher Hong <rax333j@gmail.com>
2018-02-12 05:50:50 +0800

    * Removed stylesheet from HtmlDocument temporary

--- src/client/HtmlDocument.jsx
+++ src/client/HtmlDocument.jsx
@@ -19,7 +19,8 @@ export default class HtmlDocument extends React.PureComponent {
           <title>噗比 PUPY</title>
           <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
           <link href='https://use.fontawesome.com/releases/v5.0.6/css/all.css' rel='stylesheet'/>
-          <link href='/serverSideRender.bundle.css' rel='stylesheet'/>
+          {/* TODO: Check if this still needed */}
+          {/* <link href='/serverSideRender.bundle.css' rel='stylesheet'/> */}
         </head>
         <body>
           <div id='react-root'>
