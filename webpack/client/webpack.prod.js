9ea338e0bcab6bddc297926f07d3c2a47b9cec15
Hao-Cher Hong <rax333j@gmail.com>
2018-02-11 08:21:48 +0800

    * Fixed production build settings

--- webpack/client/webpack.prod.js
+++ webpack/client/webpack.prod.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.common.js');
 
 const babelOptions = JSON.parse(fs.readFileSync(path.resolve('./.babelrc'), 'utf8'));
 
-module.exports = merge(common, {
+module.exports = {
   module: {
     rules: [
       {
@@ -20,4 +18,4 @@ module.exports = merge(common, {
   plugins: [
     new UglifyJSPlugin()
   ]
-});
+};
